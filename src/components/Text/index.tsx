878dec6fea705408f1158b57054490957e3a8af3
Christopher Cerny <cerny.christopher@gmx.at>
2023-09-26 22:10:19 +0200

    fixed some missing styles

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -6,24 +6,17 @@ import styles from './styles.css'
 
 type As = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'span' | 'div' | 'a' | typeof ReactRouterLink
 
-type TextAlign = 'left' | 'center' | 'right'
-
 type Props<T extends As> = {
-    align?: TextAlign
-    color?: 'inverse' | 'primary' | 'secondary' | 'tertiary' | 'quaternary'
-    fontWeight?: 'bold' | 'strong' | 'heavy'
-    uppercase?: boolean
     className?: string
     children: React.ReactNode
     as?: T
 } & React.ComponentPropsWithoutRef<T>
 
 const TextComponent = <T extends As = 'div'>(
-    { as, align, uppercase, className, children, color, fontWeight, ...props }: Props<T>,
+    { as, className, children, ...props }: Props<T>,
     ref: React.Ref<unknown>
 ) => {
     const hasAlias = as !== undefined
-    const resolvedTextAlign = align ?? 'left'
 
     return React.createElement(
         as ?? 'div',
@@ -31,11 +24,6 @@ const TextComponent = <T extends As = 'div'>(
             ref,
             className: cn(
                 !hasAlias && styles.defaultText,
-                uppercase && styles.uppercase,
-                resolvedTextAlign === 'right' && styles.alignRight,
-                resolvedTextAlign === 'center' && styles.alignCenter,
-                color && styles[color],
-                fontWeight && styles[fontWeight],
                 className
             ),
             ...props,
