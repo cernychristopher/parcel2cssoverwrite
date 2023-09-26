581d704a3e4f999e4a8b8446421dbedbe779ca0f
Christopher Cerny <cerny.christopher@gmx.at>
2023-09-26 20:10:04 +0200

    cleaning things we don't need

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import 'core-js/stable'
 
-import 'shared/assets/css/variables.css'
-import 'shared/assets/css/styles.css'
+import 'assets/css/variables.css'
+import 'assets/css/styles.css'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
